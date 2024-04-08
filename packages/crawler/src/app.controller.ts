8e640f8d4c1169b4c53d410f77b0845d2c22f133
tolluset <dlqud19@gmail.com>
2024-04-09 00:48:20 +0900

    feat: add get coins

--- packages/crawler/src/app.controller.ts
+++ packages/crawler/src/app.controller.ts
@@ -1,12 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('/coins')
+  getCoinMarketAll() {
+    return this.appService.getCoinMarketAll();
+  }
+
+  @Get('/coins/:coin')
+  getCoinPrice(@Param('coin') coin: string) {
+    return this.appService.getCoinPrice(coin);
+  }
+
+  @Get('/coins/:coin/days')
+  getCoinCandles(@Param('coin') coin: string) {
+    return this.appService.getCoinCandles(coin);
   }
 }
