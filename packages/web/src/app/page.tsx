7e750e95ae0d692bd6cb6544c67b752427a52c5c
tolluset <dlqud19@gmail.com>
2024-04-14 13:14:18 +0900

    feat: add login button in home

--- packages/web/src/app/page.tsx
+++ packages/web/src/app/page.tsx
@@ -1,9 +1,19 @@
-import CoinList from "./features/coin-list/CoinList";
+import { Button } from "@/components/ui/button";
+import CoinList from "./coins/coin-list";
+import Link from "next/link";
+import { ROUTES } from "@/constants";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-2 sm:p-4">
-      <CoinList />
-    </main>
+    <>
+      <nav className="flex justify-end w-full p-2 border-b sm:p-4">
+        <Button variant="outline" asChild>
+          <Link href={ROUTES.login}>로그인</Link>
+        </Button>
+      </nav>
+      <main className="flex w-full flex-col items-center justify-between pt-8 p-2 sm:p-4 sm:pt-8">
+        <CoinList />
+      </main>
+    </>
   );
 }
