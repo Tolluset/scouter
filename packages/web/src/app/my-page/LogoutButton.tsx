968d994ca9fccd3ce3e2a135a4222f1be5835b57
tolluset <dlqud19@gmail.com>
2024-04-20 06:45:13 +0900

    feat: add logout button

---
+++ packages/web/src/app/my-page/LogoutButton.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { ROUTES } from "@/constants";
+import { createClient } from "@/utils/supabase/client";
+import { useRouter } from "next/navigation";
+
+export default function LogoutButton() {
+  const router = useRouter();
+
+  const logout = async () => {
+    const client = createClient();
+    await client.auth.signOut();
+
+    router.push(ROUTES.home);
+    router.refresh();
+  };
+
+  return <Button onClick={logout}>로그아웃</Button>;
+}
