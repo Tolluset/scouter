368c290ede0c00bb9380728ada3725657a6149cb
tolluset <dlqud19@gmail.com>
2024-05-14 21:38:40 +0900

    chore: add gtm

--- packages/web/src/app/layout.tsx
+++ packages/web/src/app/layout.tsx
@@ -25,11 +25,26 @@ export default function RootLayout({
   return (
     <html lang="ko" className="h-full">
       <body className={`h-full ${pretendard.className}`}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5MC8KJ9V"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <NextTopLoader showSpinner={false} />
         {children}
         <Toaster />
         <Analytics />
       </body>
+      <Script id="g-gtm">
+        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-5MC8KJ9V');`}
+      </Script>
       <Script
         async
         src="https://www.googletagmanager.com/gtag/js?id=G-L5HP2YTK02"
